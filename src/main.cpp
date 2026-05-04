b12030c7346396a9a36fdd5573e5f29564e08d39
unknown <jakubzajac0222@gmail.com>
2026-05-05 00:33:37 +0200

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <vulkan/vulkan_raii.hpp>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+    void run() {
+		initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+	GLFWwindow* window;
+
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+		while (!glfwWindowShouldClose(window)) 
+		{
+			glfwPollEvents();
+		}
+
+    }
+
+    void cleanup() {
+		 glfwDestroyWindow(window);
+
+    	glfwTerminate();
+
+    }
+};
+
+int main()
+{
+    try
+    {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
